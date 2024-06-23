7b09a2560ace01b43b1877eb01ac50d1df76fda5
Vaibhav Singh <vaibhavsinghr.08@gmail.com>
2024-06-23 15:46:45 +0530

    Data Table with Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,21 +1,66 @@
 import React from "react";
 
-function Pagination({ currentPage, totalPages, onPageChange, totalResults }) {
+function Pagination({
+  currentPage,
+  totalPages,
+  itemsPerPage,
+  onPageChange,
+  onItemsPerPageChange,
+  totalResults,
+}) {
+  const pages = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(
+      <button
+        key={i}
+        onClick={() => onPageChange(i)}
+        className={`px-2 py-1 rounded-md ${
+          currentPage === i ? "bg-blue-500 text-white" : "bg-gray-200"
+        }`}
+      >
+        {i}
+      </button>
+    );
+  }
+
   return (
     <div className="flex justify-between items-center p-4">
       <div className="text-sm text-gray-600">
         Displaying{" "}
         <input
           type="number"
-          value={currentPage}
-          min={1}
-          max={totalPages}
-          onChange={(e) => onPageChange(Number(e.target.value))}
+          value={itemsPerPage}
+          min={10}
+          max={30}
+          onChange={(e) => onItemsPerPageChange(Number(e.target.value))}
           className="border border-gray-300 rounded-md w-16 text-center mx-1"
         />{" "}
         out of {totalResults} results
       </div>
-      <div className="flex items-center space-x-2"></div>
+      <div className="flex items-center space-x-2">
+        <button
+          className={`text-sm text-gray-700 px-2 py-1 rounded-md ${
+            currentPage === 1 ? "bg-gray-200" : "bg-blue-500 text-white"
+          }`}
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage == 1}
+        >
+          &lt; Previous
+        </button>
+        {pages.map((page) => page)}
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={`text-sm text-gray-700 px-2 py-1 rounded-md ${
+            currentPage === totalPages
+              ? "bg-gray-200"
+              : "bg-blue-500 text-white"
+          }`}
+        >
+          Next &gt;
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,11 +2,14 @@
 
 import React, { useState } from "react";
 import tableData from "../data/table_data.json";
+import Pagination from "./Pagination";
 import { LuCalendar, LuCircleDot, LuUser } from "react-icons/lu";
 import { format } from "date-fns";
 
 function Table() {
   const [selected, setSelected] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
   const handleSelect = (e, id) => {
     if (e.target.checked) {
@@ -35,82 +38,112 @@ function Table() {
     return format(date, "eee, dd MMM yyyy h:mm a");
   };
 
+  const totalPages = Math.ceil(tableData.length / itemsPerPage);
+  const currentItems = tableData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handleItemsPerPageChange = (inputItemsPerPage) => {
+    if (inputItemsPerPage >= 10 && inputItemsPerPage <= 30) {
+      setItemsPerPage(inputItemsPerPage);
+    }
+  };
+
+  const handlePageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
   return (
-    <div className="overflow-x-auto m-4 rounded-md max-h-screen">
-      <table className="min-w-full bg-white border border-gray-200 overflow-y-auto">
-        <thead>
-          <tr className="text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-            <th className="px-4 py-3 border-b">
-              <input
-                type="checkbox"
-                checked={selected.length === tableData.length}
-                onChange={handleSelectAll}
-              />
-            </th>
-            <th className="px-4 py-3 border-b">
-              <div className="flex items-center whitespace-nowrap">
-                <LuCalendar className="mr-2" /> Created On
-              </div>
-            </th>
-            <th className="px-4 py-3 border-b">
-              <div className="flex items-center whitespace-nowrap">
-                <LuUser className="mr-2" /> Payer
-              </div>
-            </th>
-            <th className="px-4 py-3 border-b">
-              <div className="flex items-center whitespace-nowrap">
-                <LuCircleDot className="mr-2" /> Status
-              </div>
-            </th>
-            <th className="px-4 py-3 border-b"># Email</th>
-            <th className="px-4 py-3 border-b"># Payer Phone</th>
-            <th className="px-4 py-3 border-b"># Services</th>
-            <th className="px-4 py-3 border-b">
-              <div className="flex items-center whitespace-nowrap">
-                <LuCalendar className="mr-2" /> Scheduled
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="text-sm text-gray-700">
-          {tableData.map((item) => (
-            <tr key={item.id} className="hover:bg-gray-50">
-              <td className="px-4 py-3 border-b">
+    <div className="flex flex-col h-full">
+      <div className="overflow-x-auto m-4 rounded-md flex-grow">
+        <table className="min-w-full bg-white border border-gray-200 overflow-y-auto">
+          <thead>
+            <tr className="text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+              <th className="px-4 py-3 border-b">
                 <input
                   type="checkbox"
-                  checked={selected.includes(item.id)}
-                  onChange={(event) => handleSelect(event, item.id)}
+                  checked={selected.length === tableData.length}
+                  onChange={handleSelectAll}
                 />
-              </td>
-              <td className="px-4 py-3 border-b whitespace-nowrap">
-                {formatDate(item.createdOn)}
-              </td>
-              <td className="px-4 py-3 border-b">{item.payer}</td>
-              <td className="px-4 py-3 border-b whitespace-nowrap">
-                <span
-                  className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                    statusClasses[item.status]
-                  }`}
-                >
-                  {item.status}
-                </span>
-              </td>
-              <td className="px-4 py-3 border-b whitespace-nowrap">
-                {item.email}
-              </td>
-              <td className="px-4 py-3 border-b whitespace-nowrap">
-                {item.phone}
-              </td>
-              <td className="px-4 py-3 border-b whitespace-nowrap">
-                {item.services}
-              </td>
-              <td className="px-4 py-3 border-b whitespace-nowrap">
-                {formatDate(item.scheduled)}
-              </td>
+              </th>
+              <th className="px-4 py-3 border-b">
+                <div className="flex items-center whitespace-nowrap">
+                  <LuCalendar className="mr-2" /> Created On
+                </div>
+              </th>
+              <th className="px-4 py-3 border-b">
+                <div className="flex items-center whitespace-nowrap">
+                  <LuUser className="mr-2" /> Payer
+                </div>
+              </th>
+              <th className="px-4 py-3 border-b">
+                <div className="flex items-center whitespace-nowrap">
+                  <LuCircleDot className="mr-2" /> Status
+                </div>
+              </th>
+              <th className="px-4 py-3 border-b"># Email</th>
+              <th className="px-4 py-3 border-b"># Payer Phone</th>
+              <th className="px-4 py-3 border-b"># Services</th>
+              <th className="px-4 py-3 border-b">
+                <div className="flex items-center whitespace-nowrap">
+                  <LuCalendar className="mr-2" /> Scheduled
+                </div>
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="text-sm text-gray-700">
+            {currentItems.map((item) => (
+              <tr key={item.id} className="hover:bg-gray-50">
+                <td className="px-4 py-3 border-b">
+                  <input
+                    type="checkbox"
+                    checked={selected.includes(item.id)}
+                    onChange={(event) => handleSelect(event, item.id)}
+                  />
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  {formatDate(item.createdOn)}
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  {item.payer}
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  <span
+                    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
+                      statusClasses[item.status]
+                    }`}
+                  >
+                    {item.status}
+                  </span>
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  {item.email}
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  {item.phone}
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  {item.services}
+                </td>
+                <td className="px-4 py-3 border-b whitespace-nowrap">
+                  {formatDate(item.scheduled)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        itemsPerPage={itemsPerPage}
+        onItemsPerPageChange={handleItemsPerPageChange}
+        onPageChange={handlePageChange}
+        totalResults={tableData.length}
+      />
     </div>
   );
 }
